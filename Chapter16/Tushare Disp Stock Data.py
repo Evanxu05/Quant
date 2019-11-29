70341c7a23491691fb9e7665fe56e4e1b37ec6f4
evanxu05 <evanxu05@qq.com>
2019-11-29 12:51:53 +0800

    git commit -m '20191129'

--- Chapter16/Tushare Disp Stock Data.py
+++ Chapter16/Tushare Disp Stock Data.py
@@ -15,15 +15,15 @@ import tushare as ts
 ts.set_token('fd5caf31a54962a7509e7d4d7972d235dbbc4f0c583003d0ef22d2cf')
 pro = ts.pro_api()
 
-plt.rcParams['font.sans-serif']=['Arial Unicode MS'] #用来正常显示中文标签
+plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
 plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
 
 #股票基础信息
 stock_code = "000016.SH"#设置股票代码
 End_date = datetime.date.today()
-Start_date = End_date-timedelta(365)
+Start_date = End_date-timedelta(40)
 
-df_stockload = ts.pro_bar(ts_code=stock_code, asset='I', start_date=Start_date.strftime('%Y%m%d'), end_date=End_date.strftime('%Y%m%d'),freq='15min',ma=[4, 8, 32,64])
+df_stockload = ts.pro_bar(ts_code=stock_code, asset='I', start_date=Start_date.strftime('%Y%m%d'), end_date=End_date.strftime('%Y%m%d'),freq='15min',ma=[8, 16, 48,96])
 df_stockload = df_stockload.sort_index(ascending=False)#降序排序
 #df_stockload = df_stockload.sort_index()#升序排序
 
@@ -35,7 +35,7 @@ print (df_stockload.describe())#查看各列数据描述性统计
 
 
 
-fig = plt.figure(figsize=(12,8), dpi=100,facecolor="white")#创建fig对象
+fig = plt.figure(figsize=(14,8), dpi=100,facecolor="white")#创建fig对象
 #fig.subplots_adjust(left=0.09,bottom=0.20, right=0.94,top=0.90, wspace=0.2, hspace=0)
 #graph_KAV = fig.add_subplot(1,1,1)#创建子图
 
@@ -43,8 +43,8 @@ gs = gridspec.GridSpec(5, 1, left=0.05, bottom=0.2, right=0.96, top=0.96, wspace
 graph_KAV = fig.add_subplot(gs[0,:])
 graph_VOL = fig.add_subplot(gs[1,:])
 graph_MACD = fig.add_subplot(gs[2,:])
-graph_MACD8 = fig.add_subplot(gs[3,:])
-graph_MACD64 = fig.add_subplot(gs[4,:])
+graph_MACD1 = fig.add_subplot(gs[3,:])
+graph_MACD2 = fig.add_subplot(gs[4,:])
 
 """ 绘制K线图 """
 #方法1
@@ -61,10 +61,10 @@ mpf.candlestick_ochl(graph_KAV, ohlc, width=0.2, colorup='r', colordown='g', alp
 numt = np.arange(0, len(df_stockload.index))
 
 #绘制均线走势    
-graph_KAV.plot(numt, df_stockload['ma4'],'black', label='M4',lw=1.0)
-graph_KAV.plot(numt, df_stockload['ma8'],'green',label='M8', lw=1.0)
-graph_KAV.plot(numt, df_stockload['ma32'],'blue',label='M32', lw=1.0)
-graph_KAV.plot(numt, df_stockload['ma64'],'yellow',label='M64', lw=1.0)
+graph_KAV.plot(numt, df_stockload['ma8'],'black', label='M8',lw=1.0)
+graph_KAV.plot(numt, df_stockload['ma16'],'green',label='M16', lw=1.0)
+graph_KAV.plot(numt, df_stockload['ma48'],'blue',label='M48', lw=1.0)
+graph_KAV.plot(numt, df_stockload['ma96'],'yellow',label='M96', lw=1.0)
 graph_KAV.legend(loc='best')
 
 """ 绘制移动平均线图 """
@@ -94,7 +94,7 @@ graph_VOL.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定 每15
 
 ''' 绘制MACD '''   
          
-macd_dif, macd_dea, macd_bar = talib.MACD(df_stockload['close'].values, fastperiod=8, slowperiod=16, signalperiod=6)
+macd_dif, macd_dea, macd_bar = talib.MACD(df_stockload['close'].values, fastperiod=16, slowperiod=32, signalperiod=12)
 graph_MACD.plot(np.arange(0, len(df_stockload.index)), macd_dif, 'red', label='macd dif') #dif    
 graph_MACD.plot(np.arange(0, len(df_stockload.index)), macd_dea, 'blue', label='macd dea') #dea 
 #绘制BAR>0 柱状图
@@ -113,47 +113,46 @@ graph_MACD.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定 每1
 
 ''' 绘制MACD ''' 
 
-''' 绘制MACD8 '''   
+''' 绘制MACD1 '''   
          
-macd_dif8, macd_dea8, macd_bar8 = talib.MACD(df_stockload['close'].values, fastperiod=4, slowperiod=8, signalperiod=3)
-graph_MACD8.plot(np.arange(0, len(df_stockload.index)), macd_dif8, 'red', label='macd dif8') #dif    
-graph_MACD8.plot(np.arange(0, len(df_stockload.index)), macd_dea8, 'blue', label='macd dea8') #dea 
+macd_dif1, macd_dea1, macd_bar1 = talib.MACD(df_stockload['close'].values, fastperiod=8, slowperiod=16, signalperiod=6)
+graph_MACD1.plot(np.arange(0, len(df_stockload.index)), macd_dif1, 'red', label='macd dif1') #dif    
+graph_MACD1.plot(np.arange(0, len(df_stockload.index)), macd_dea1, 'blue', label='macd dea1') #dea 
 #绘制BAR>0 柱状图
-bar_red8 = np.where(macd_bar8>0, 2*macd_bar8, 0)
+bar_red1 = np.where(macd_bar1>0, 2*macd_bar1, 0)
 #绘制BAR<0 柱状图
-bar_green8 = np.where(macd_bar8<0, 2*macd_bar8, 0)        
-graph_MACD8.bar(np.arange(0, len(df_stockload.index)), bar_red8, facecolor='red')
-graph_MACD8.bar(np.arange(0, len(df_stockload.index)), bar_green8, facecolor='green')
-graph_MACD8.legend(loc='best',shadow=True, fontsize ='10')
+bar_green1 = np.where(macd_bar1<0, 2*macd_bar1, 0) 
+graph_MACD1.bar(np.arange(0, len(df_stockload.index)), bar_green1, facecolor='green')
+graph_MACD1.legend(loc='best',shadow=True, fontsize ='10')
 
-graph_MACD8.set_ylabel(u"MACD")
+graph_MACD1.set_ylabel(u"MACD")
 #graph_MACD.set_xlabel("日期")
-graph_MACD8.set_xlim(0,len(df_stockload.index)) #设置一下x轴的范围
-graph_MACD8.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定 每15天标一个日期
+graph_MACD1.set_xlim(0,len(df_stockload.index)) #设置一下x轴的范围
+graph_MACD1.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定 每15天标一个日期
 #graph_MACD.set_xticklabels([df_stockload.index.strftime('%Y-%m-%d')[index] for index in graph_MACD.get_xticks()])#标签设置为日期
 
-''' 绘制MACD8 ''' 
+''' 绘制MACD1 ''' 
 
-''' 绘制MACD64 '''   
+''' 绘制MACD2 '''   
          
-macd_dif64, macd_dea64, macd_bar64 = talib.MACD(df_stockload['close'].values, fastperiod=32, slowperiod=64, signalperiod=24)
-graph_MACD64.plot(np.arange(0, len(df_stockload.index)), macd_dif64, 'red', label='macd dif64') #dif    
-graph_MACD64.plot(np.arange(0, len(df_stockload.index)), macd_dea64, 'blue', label='macd dea64') #dea 
+macd_dif2, macd_dea2, macd_bar2 = talib.MACD(df_stockload['close'].values, fastperiod=48, slowperiod=96, signalperiod=36)
+graph_MACD2.plot(np.arange(0, len(df_stockload.index)), macd_dif2, 'red', label='macd dif2') #dif    
+graph_MACD2.plot(np.arange(0, len(df_stockload.index)), macd_dea2, 'blue', label='macd dea2') #dea 
 #绘制BAR>0 柱状图
-bar_red64 = np.where(macd_bar64>0, 2*macd_bar64, 0)
+bar_red2 = np.where(macd_bar2>0, 2*macd_bar2, 0)
 #绘制BAR<0 柱状图
-bar_green64 = np.where(macd_bar64<0, 2*macd_bar64, 0)        
-graph_MACD64.bar(np.arange(0, len(df_stockload.index)), bar_red64, facecolor='red')
-graph_MACD64.bar(np.arange(0, len(df_stockload.index)), bar_green64, facecolor='green')
-graph_MACD64.legend(loc='best',shadow=True, fontsize ='10')
+bar_green2 = np.where(macd_bar2<0, 2*macd_bar2, 0)        
+graph_MACD2.bar(np.arange(0, len(df_stockload.index)), bar_red2, facecolor='red')
+graph_MACD2.bar(np.arange(0, len(df_stockload.index)), bar_green2, facecolor='green')
+graph_MACD2.legend(loc='best',shadow=True, fontsize ='10')
 
-graph_MACD64.set_ylabel(u"MACD")
+graph_MACD2.set_ylabel(u"MACD")
 #graph_MACD.set_xlabel("日期")
-graph_MACD64.set_xlim(0,len(df_stockload.index)) #设置一下x轴的范围
-graph_MACD64.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定 每15天标一个日期
+graph_MACD2.set_xlim(0,len(df_stockload.index)) #设置一下x轴的范围
+graph_MACD2.set_xticks(range(0,len(df_stockload.index),15))#X轴刻度设定 每15天标一个日期
 #graph_MACD.set_xticklabels([df_stockload.index.strftime('%Y-%m-%d')[index] for index in graph_MACD.get_xticks()])#标签设置为日期
 
-''' 绘制MACD8 ''' 
+''' 绘制MACD2 ''' 
 
 
 
@@ -175,13 +174,13 @@ for label in graph_MACD.xaxis.get_ticklabels():
 	#label.set_fontsize(10)#设置标签字体
 	label.set_visible(False)
 
-for label in graph_MACD8.xaxis.get_ticklabels():   
+for label in graph_MACD1.xaxis.get_ticklabels():   
 	#label.set_rotation(45)
 	#label.set_fontsize(10)#设置标签字体
 	label.set_visible(False)		
 
 
-for label in graph_MACD64.xaxis.get_ticklabels():   
+for label in graph_MACD2.xaxis.get_ticklabels():   
 	#label.set_rotation(45)
 	#label.set_fontsize(10)#设置标签字体
 	label.set_visible(False)
